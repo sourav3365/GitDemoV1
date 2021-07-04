023ccf42d4d9cf8be3a5a9c4bd63cb6d9928a9c7
sourav3365 <82954143+sourav3365@users.noreply.github.com>
2021-07-04 17:33:10 +0530

    First Commit

---
+++ src/AAA.java
@@ -0,0 +1,24 @@
+
+public class AAA {
+	
+	int a = 10;
+	int b = 20;
+	
+}
+
+
+class BBB{
+	
+	
+	public static void main(String[] args) {
+		
+		AAA a = new AAA();
+		
+		System.out.println(a);
+		
+	}
+}
+
+final class String{
+	
+}
