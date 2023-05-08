b0ef40d0e3db952e1f8b96d56483c8746964ffe4
Sourav Choudhury <sourav3365@gmail.com>
2023-05-08 19:25:22 +0530

    added new function

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 def function():
     print("This function does not do much thing")
     pass
-
+def adding():
+    print("Adding a completely new function")
 if __name__=="__main__":
 
     "Some changes in the feature Branch"
